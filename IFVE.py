3b9265fd748b5325aa1ab70a98253ca0b83ed392
NevyHallTV28 <nevhall.14@gmail.com>
2025-04-15 10:19:13 -0700

    okay

--- IFVE.py
+++ IFVE.py
@@ -1,19 +1,16 @@
-#Init Frame vs from 
 from PyUI.Screen import Screen
 from PyUI.PageElements import *
-
 class Ifve(Screen):
     def __init__(self, window):
         super().__init__(window, (10, 0, 145))
         self.elements = [
         ]
-        #loop to setup board
+        #create board with a range 
         index = -1
         for x in range(8):
             index += 1
             coordX = 11*x + 15                     
             self.elements.append(ImageClass((coordX, 50), 10, 10, "./imgs/picture.png", index))
-
 class ImageClass(Image):
     def __init__(self, centerXY, width, height, imagePath, index):
         super().__init__(centerXY, width, height, imagePath)
