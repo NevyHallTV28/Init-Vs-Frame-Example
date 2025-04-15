2a9a4340ff1cea0ad1fa4530280da54da7c7e6a6
NevyHallTV28 <nevhall.14@gmail.com>
2025-04-15 10:01:18 -0700

    okat ne

--- IFVE.py
+++ IFVE.py
@@ -1,2 +1,25 @@
-#Init Frame vs Example
-def initFrame ()
+#Init Frame vs from 
+from PyUI.Screen import Screen
+from PyUI.PageElements import *
+
+class Example(Screen):
+    def __init__(self, window):
+        super().__init__(window, (10, 0, 145))
+        self.elements = [
+        ]
+
+        #loop to setup board
+        index = -1
+        for x in range(8):
+            index += 1
+            coordX = 11*x + 15                     
+            self.elements.append(CoolImage((coordX, 50), 10, 10, "./imgs/Jfat.jpg.png", index))
+
+
+class CoolImage(Image):
+    def __init__(self, centerXY, width, height, imagePath, index):
+        super().__init__(centerXY, width, height, imagePath)
+        self.index = index
+
+    def onClick(self, screen):
+        screen.elements.remove(self)
